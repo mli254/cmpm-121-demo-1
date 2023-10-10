4f7ad1aae24834b22061a31aeee30688d6798058
mli254 <mli254@ucsc.edu>
2023-10-10 04:03:57 -0700

    step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,11 @@ const button = document.createElement("button");
 button.innerHTML = buttonName;
 app.append(button);
 
+const squid = document.createElement("button");
+squid.innerHTML = "🦑 Squid As Bait | Cost: 10 Fish";
+squid.disabled = true;
+app.append(squid);
+
 // Adding counter incrementing on click
 let counter: number = 0;
 const counterMsg: HTMLDivElement = document.createElement("div");
@@ -30,20 +35,35 @@ button.addEventListener("click", () => {
   }
 });
 
-// Adding auto-increment of the button using animate
+squid.addEventListener("click", () => {
+  purchase(10, 1);
+});
 
+// Adding auto-increment of the button using animate
 let last = 0;
+let growth = 0;
 autoButton();
 
 // Functions
 function autoButton() {
   if (performance.now() - last >= 1000) {
     last = performance.now();
-    counterMsg.innerHTML = `You have caught ${(counter += 1)} fish!`;
+    counterMsg.innerHTML = `You have caught ${(counter += growth)} fish!`;
+  }
+
+  if (counter >= 10) {
+    squid.disabled = false;
+  } else if (counter < 10) {
+    squid.disabled = true;
   }
   requestAnimationFrame(autoButton);
 }
 
 function incrementButton() {
   counterMsg.innerHTML = `You have caught ${(counter += 1)} fish!`;
+}
+
+function purchase(price: number, growthRate: number) {
+  counterMsg.innerHTML = `You have caught ${(counter -= price)} fish!`;
+  growth += growthRate;
 }
