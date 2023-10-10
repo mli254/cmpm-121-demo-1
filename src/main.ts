f8a3c853ece50db6ee354d4348f62cd3a2fd4978
mli254 <mli254@ucsc.edu>
2023-10-10 00:47:30 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Gone Fishing"; // changed name
+const buttonName = "🎣 Time to fish!"; // name of button
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = buttonName;
+app.append(button);
